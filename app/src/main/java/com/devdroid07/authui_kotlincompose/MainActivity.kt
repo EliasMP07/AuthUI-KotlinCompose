b446c87337354c41cb0edaff38ce1b7a11ebbb5f
= <117881469+EliasMP07@users.noreply.github.com>
2024-07-29 18:49:50 -0600

    Dagger Hilt Configuration

--- app/src/main/java/com/devdroid07/authui_kotlincompose/MainActivity.kt
+++ app/src/main/java/com/devdroid07/authui_kotlincompose/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.devdroid07.authui_kotlincompose.core.presentation.designsystem.theme.AuthUIKotlinComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
